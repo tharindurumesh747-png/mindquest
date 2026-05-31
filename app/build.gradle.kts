dda67ef5438cd51e6db724c21099ce5cc1b8b082
tharindurumesh747-png <tharindurumesh747@gmail.com>
2026-05-31 17:17:19 +0530

    Remove release and debug signing configurations
    Removed release signing configuration and debug signing configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,13 +21,6 @@ android {
   }
 
   signingConfigs {
-    create("release") {
-      val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
-      storeFile = file(keystorePath)
-      storePassword = System.getenv("STORE_PASSWORD")
-      keyAlias = "upload"
-      keyPassword = System.getenv("KEY_PASSWORD")
-    }
     create("debugConfig") {
       storeFile = file("${rootDir}/debug.keystore")
       storePassword = "android"
@@ -43,9 +36,7 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
-    }
+   
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
