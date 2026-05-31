51938b4000414a3a7b89846d01f3f9835c57baea
tharindurumesh747-png <tharindurumesh747@gmail.com>
2026-05-31 15:44:29 +0530

    Update compileSdk and targetSdk to version 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk = 35
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "com.aistudio.mindquest.zfkpwt"
     minSdk = 24
-    targetSdk = 35
+    targetSdk = 36
     versionCode = 1
     versionName = "1.0"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
